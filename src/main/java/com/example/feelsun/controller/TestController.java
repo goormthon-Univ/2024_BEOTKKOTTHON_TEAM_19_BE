01c15b847e6089d4edfe7c00125275f130292c4a
Jin_00 <anjdal64@gmail.com>
2024-03-14 19:21:13 +0900

    [test] #7 - login 인증 후 jwt 토큰 테스트 컨트롤러 작성

--- src/main/java/com/example/feelsun/controller/TestController.java
+++ src/main/java/com/example/feelsun/controller/TestController.java
@@ -1,2 +1,22 @@
-package com.example.feelsun.controller;public class TestController {
+package com.example.feelsun.controller;
+
+import com.example.feelsun.config.auth.PrincipalUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class TestController {
+
+    @RequestMapping("/test")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<?> tokenTest(@AuthenticationPrincipal PrincipalUserDetails principalUserDetails) {
+        return ResponseEntity.status(HttpStatus.OK).body(principalUserDetails.getUser());
+    }
 }
