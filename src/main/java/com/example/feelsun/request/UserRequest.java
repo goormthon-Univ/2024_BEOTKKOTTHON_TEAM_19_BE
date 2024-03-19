919a7e756e64b6cf7d0a23a27dea3b01dde13866
Jin_00 <anjdal64@gmail.com>
2024-03-20 00:21:01 +0900

    [feat] #13 - 회원가입시 필수값 설정

--- src/main/java/com/example/feelsun/request/UserRequest.java
+++ src/main/java/com/example/feelsun/request/UserRequest.java
@@ -3,6 +3,8 @@ package com.example.feelsun.request;
 import com.example.feelsun.domain.User;
 import com.example.feelsun.domain.UserEnum;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,9 +15,13 @@ public class UserRequest {
     public static class UserSignUpRequest {
 
         @NotNull(message = "유저 아이디는 필수 입력 값입니다.")
+        @Size(min = 6, max = 20, message = "아이디는 6자 이상 20자 이하로 입력해주세요.")
         private String username;
+
         @NotNull(message = "비밀번호는 필수 입력 값입니다.")
+        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*\\W).{8,20}$", message = "비밀번호는 8자 이상 20자 이내이며, 문자, 숫자, 특수문자를 각각 최소 1개 이상 포함해야 합니다.")
         private String password;
+
         @NotNull(message = "닉네임은 필수 입력 값입니다.")
         private String nickname;
 
