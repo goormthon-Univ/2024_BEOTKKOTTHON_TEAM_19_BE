1916fe9e157b4af2272ccdb8f561cb1dbd7dbfb3
Jin_00 <anjdal64@gmail.com>
2024-03-20 00:36:30 +0900

    [refactor] #13 - 리프래쉬 토큰 바디로 받도록 코드 수정

--- src/main/java/com/example/feelsun/controller/AuthController.java
+++ src/main/java/com/example/feelsun/controller/AuthController.java
@@ -6,17 +6,19 @@ import com.example.feelsun.config.jwt.refreshToken.RefreshTokenResponse.*;
 import com.example.feelsun.config.utils.ApiResponseBuilder;
 import com.example.feelsun.domain.User;
 import com.example.feelsun.repository.UserJpaRepository;
+import com.example.feelsun.request.AuthRequest.*;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,28 +31,21 @@ public class AuthController {
     private final JwtProvider jwtProvider;
     private final UserJpaRepository userJpaRepository;
 
-    private String extractRefreshTokenFromCookies(HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if ("refresh-token".equals(cookie.getName())) {
-                    return cookie.getValue();
-                }
-            }
-        }
-        return null; // 쿠키에서 리프래쉬 토큰을 찾을 수 없는 경우
-    }
-
     @Operation(summary = "토큰 재발급", description = "토큰 재발급을 진행합니다.")
     @ApiResponse(responseCode = "200", description = "토큰 재발급 성공",
             content = @Content(mediaType = "application/json",
                     schema = @Schema(implementation = RefreshTokenResponseDTO.class)))
     @PostMapping("/refresh-token")
-    public ResponseEntity<?> refreshToken(HttpServletRequest request) {
-        String refreshToken = extractRefreshTokenFromCookies(request);
+    public ResponseEntity<?> refreshToken(@RequestBody @Valid RefreshTokenRequest requestDTO, Errors errors) {
+        String refreshToken = requestDTO.getRefreshToken();
 
-        if (refreshToken == null || !jwtProvider.validateRefreshToken(refreshToken)) {
-            throw new Exception401("리프래쉬 토큰이 유효하지 않습니다.");
+        if (refreshToken == null) {
+            throw new Exception401("리프래쉬 토큰이 존재하지 않습니다.");
+        }
+
+        // refresh token 유효성 검사
+        if (!jwtProvider.validateRefreshToken(refreshToken)) {
+            throw new Exception401("유효하지 않은 리프래쉬 토큰입니다.");
         }
 
         String userId = jwtProvider.getIdentity(refreshToken);
