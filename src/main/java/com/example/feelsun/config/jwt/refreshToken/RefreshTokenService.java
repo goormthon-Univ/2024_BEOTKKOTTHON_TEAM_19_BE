42320541c4c2272982a94cbf3e83ec62d0f1904f
Jin_00 <anjdal64@gmail.com>
2024-03-21 15:34:20 +0900

    [refactor] #18 - 액세스 토큰 재생성 코드 리팩토링
    기존에는 accessToken 만 전달해주었지만, 로그인했을 때와 동일한 정보를 담아서 전달하도록 코드를 수정해주었음
    
    그리고 service 로직으로 핵심 로직을 옮겼음

--- src/main/java/com/example/feelsun/config/jwt/refreshToken/RefreshTokenService.java
+++ src/main/java/com/example/feelsun/config/jwt/refreshToken/RefreshTokenService.java
@@ -1,7 +1,12 @@
 package com.example.feelsun.config.jwt.refreshToken;
 
 
+import com.example.feelsun.config.errors.exception.Exception401;
 import com.example.feelsun.config.jwt.JwtProvider;
+import com.example.feelsun.domain.User;
+import com.example.feelsun.repository.UserJpaRepository;
+import com.example.feelsun.request.AuthRequest;
+import com.example.feelsun.response.UserResponse.*;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,9 +18,13 @@ import java.time.Duration;
 public class RefreshTokenService {
 
     private final RedisTemplate<String, Object> redisTemplate;
+    private final JwtProvider jwtProvider;
+    private final UserJpaRepository userJpaRepository;
 
-    public RefreshTokenService(RedisTemplate<String, Object> redisTemplate) {
+    public RefreshTokenService(RedisTemplate<String, Object> redisTemplate, JwtProvider jwtProvider, UserJpaRepository userJpaRepository) {
         this.redisTemplate = redisTemplate;
+        this.jwtProvider = jwtProvider;
+        this.userJpaRepository = userJpaRepository;
     }
 
     // 리프래쉬 토큰 저장
@@ -29,5 +38,31 @@ public class RefreshTokenService {
     public void deleteRefreshToken(String identity) {
         redisTemplate.delete(identity);
     }
+
+    @Transactional
+    public UserLoginResponseWithToken generateToken(AuthRequest.RefreshTokenRequest requestDTO) {
+        String refreshToken = requestDTO.getRefreshToken();
+
+        if (refreshToken == null) {
+            throw new Exception401("리프래쉬 토큰이 존재하지 않습니다.");
+        }
+
+        // refresh token 유효성 검사
+        if (!jwtProvider.validateRefreshToken(refreshToken)) {
+            throw new Exception401("유효하지 않은 리프래쉬 토큰입니다.");
+        }
+
+        String userId = jwtProvider.getIdentity(refreshToken);
+
+        User user = userJpaRepository.findById(Long.parseLong(userId))
+                .orElseThrow(() -> new Exception401("유저를 찾을 수 없습니다."));
+
+        String newAccessToken = jwtProvider.createToken(user.getId().toString(), user.getRole().toString(), user.getNickname());
+
+        UserLoginResponse loginResponseDTO = new UserLoginResponse(user.getId(), user.getUsername(), user.getNickname());
+
+        return new UserLoginResponseWithToken(loginResponseDTO, newAccessToken, refreshToken);
+    }
+
 
 }
