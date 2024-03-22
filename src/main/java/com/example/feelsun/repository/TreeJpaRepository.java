7577dbce40ac2a9e83b60d46264ebb5f362c529d
imjanghyeok <qkdrhkgn23@naver.com>
2024-03-23 07:08:09 +0900

    "feat: Tree Repo 추가 (유저의 습관 리스트)"

--- src/main/java/com/example/feelsun/repository/TreeJpaRepository.java
+++ src/main/java/com/example/feelsun/repository/TreeJpaRepository.java
@@ -1,16 +1,16 @@
 package com.example.feelsun.repository;
 
 import com.example.feelsun.domain.Tree;
+import com.example.feelsun.domain.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TreeJpaRepository extends JpaRepository<Tree, Integer> {
 
     Page<Tree> findAll(Specification<Tree> spec, Pageable pageable);
+    List<Tree> findByUser(User user);
 }
